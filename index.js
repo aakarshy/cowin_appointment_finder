11a39a4ea6f4bfb60737f18c141be74602273596
aakarshy <aakarshyadav.iitkgp@gmail.com>
2021-05-11 10:07:52 +0530

    sldndsldn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app   = express();
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const cors = require('cors');
+var moment = require('moment');
 
 app.use(cors());
 
@@ -32,7 +33,8 @@ const https = require('https');
 app.get("/",async function(req,res){
 	//const appointments = await axios.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=312&date=07-05-2021');
 	//console.log(appointments);
-  var id = '312', date='09-05-2021', age=45;
+
+  var id = '312', date=moment().format('DD-MM-YYYY'), age=45;
   if(req.query.id)
     id = req.query.id;
   if(req.query.date)
@@ -51,32 +53,33 @@ https.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendar
   resp.on('end', () => {
 
     f = data;
-    //f= JSON.parse(f);
-    //console.log(f);
-    //for(var i of f.centers)
-    //{
-    //	//console.log(i);
-    //	for(var j of i.sessions)
-    //	{
-    //		let obj={};
-    //		if(j.min_age_limit==age && j.available_capacity>0)
-    //		{
-    //			obj={
-    //				NAME: i.name,
-    //				ADDRESS: i.address,
-    //				BLOCK: i.block_name,
-    //				DATE: j.date,
-    //				AVAILABLE: j.available_capacity,
-    //				VACCINE: j.vaccine,
-    //				SLOTS: j.slots
-    //			}
-    //		}
-    //		else continue;
-    //		results.push(obj)
-    //	}
-    //}
-    console.log(results)
-  	res.render("home",{results:data});
+    f= JSON.parse(f);
+    console.log(f);
+    for(var i of f.centers)
+    {
+    	//console.log(i);
+    	for(var j of i.sessions)
+    	{
+    		let obj={};
+    		if(j.min_age_limit==age && j.available_capacity>0)
+    		{
+    			obj={
+            DISTRICT: i.district_name,
+            STATE: i.state_name,
+    				NAME: i.name,
+    				ADDRESS: i.address,
+    				BLOCK: i.block_name,
+    				DATE: j.date,
+    				AVAILABLE: j.available_capacity,
+    				VACCINE: j.vaccine,
+    				SLOTS: j.slots
+    			}
+    		}
+    		else continue;
+    		results.push(obj)
+    	}
+    }
+  	res.render("home",{results:results});
   });
   
 
