af4797484abe66fee44104d85a54725ddb506fcb
aakarshy <aakarshyadav.iitkgp@gmail.com>
2021-05-12 21:16:29 +0530

    ginongs

--- index.js
+++ index.js
@@ -29,32 +29,37 @@ app.options("*", cors());
 
 const https = require('https');
 
-
 app.get("/",async function(req,res){
 	//const appointments = await axios.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=312&date=07-05-2021');
 	//console.log(appointments);
-
-  var id = '312', date=moment().format('DD-MM-YYYY'), age=45;
+  var id = 312, age=18; //date=moment(date).format('DD-MM-YY');
   if(req.query.id)
     id = req.query.id;
-  if(req.query.date)
-    date=req.query.date;
+ // if(req.query.date)
+   // date=req.query.date;
   if(req.query.age)
     age=req.query.age;
-https.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id='+id+'&date='+date, (resp) => {
- let data=' ',f,results=[];
+var completed_requests = 0;
+var results=[];
+//for(id = 0; id<=737;++id){
+for(var k=0;k<28;k=k+7){
+  var date=moment().add(k,'days').format('DD-MM-YYYY')
+  console.log(date);
+  console.log(id);
+  await https.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id='+id+'&date='+date, (resp) => {
+    var data=' ',f;
 
   // A chunk of data has been received.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
-
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+  
+  completed_requests = completed_requests + 1;
+  //console.log(completed_requests);
   // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-
+ resp.on('end', () => {
     f = data;
     f= JSON.parse(f);
-    console.log(f);
     for(var i of f.centers)
     {
     	//console.log(i);
@@ -79,15 +84,17 @@ https.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendar
     		results.push(obj)
     	}
     }
-  	res.render("home",{results:results});
+     if (completed_requests==4){
+  	   console.log(results);
+       res.render("home",{results:results});
+     }
+     
   });
-  
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
+})//.on("error", (err) => {
+  //console.log("Error: " + err.message);
+//})
+}
 })
-});
-
 app.listen(process.env.PORT || 3000, ()=>{
 	console.log("Server up on Port 3000")
-});
+})
